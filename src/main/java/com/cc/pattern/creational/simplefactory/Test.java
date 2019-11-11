d5f6f6911dba69cd38c9effeea877fddd556653e
chengcheng1 <chengcheng1@yidian-inc.com>
2019-11-11 17:27:25 +0800

    add simple factory

--- src/main/java/com/cc/pattern/creational/simplefactory/Test.java
+++ src/main/java/com/cc/pattern/creational/simplefactory/Test.java
@@ -2,7 +2,13 @@ package com.cc.pattern.creational.simplefactory;
 
 public class Test {
     public static void main(String[] args) {
-        Video video = new JavaVideo();
+        VideoFactory videoFactory = new VideoFactory();
+
+        //Video video = videoFactory.getVideo("java");
+        Video video = videoFactory.getVideo(JavaVideo.class);
+        if(video == null){
+            return;
+        }
         video.produce();
     }
 }
