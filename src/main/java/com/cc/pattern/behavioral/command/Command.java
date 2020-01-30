83baa40c416594d373a4b5aa1e419bfeb832c0ac
chengcheng1 <chengcheng1@yidian-inc.com>
2020-01-30 15:57:21 +0800

    add command, interpreter, memnote pattern

---
+++ src/main/java/com/cc/pattern/behavioral/command/Command.java
@@ -0,0 +1,5 @@
+package com.cc.pattern.behavioral.command;
+
+public interface Command {
+    void execute();
+}
---
+++ src/main/java/com/cc/pattern/behavioral/interpreter/Interpreter.java
@@ -0,0 +1,8 @@
+package com.cc.pattern.behavioral.interpreter;
+
+/**
+ * Created by geely.
+ */
+public interface Interpreter {
+    int interpret();
+}
